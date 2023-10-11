e5187d3cd0b25d1babdd8460151a8df4488f1dff
Alan Muñoz <alanjmr206@gmail.com>
2023-10-11 14:39:48 -0700

    add post user method

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,73 @@
 const { onRequest } = require("firebase-functions/v2/https");
 //const logger = require("firebase-functions/logger");
-const admin = require('firebase-admin');
-const express = require('express');
+const admin = require("firebase-admin");
+const express = require("express");
 
 const app = express();
 admin.initializeApp({
-    credential: admin.credential.cert('./credentials.json')
-})
+  credential: admin.credential.cert("./credentials.json"),
+});
 
 const db = admin.firestore();
 
-app.get('/hello-world', (req, res) => {
-    return res.status(200).json({ message: 'hello world' })
-})
+app.get("/hello-world", (req, res) => {
+  return res.status(200).json({ message: "hello world" });
+});
 
-app.post('/api/product', async (req, res) => {
-    try {
-        await db.collection('products')
-            .doc('/' + req.body.id + '/')
-            .create({ name: req.body.name })
-        return res.status(204).json();
-    } catch (err) {
-        console.log(err)
-        return res.status(500).send(err);
-    }
-})
+app.post("/api/product", async (req, res) => {
+  try {
+    await db
+      .collection("products")
+      .doc("/" + req.body.id + "/")
+      .create({ name: req.body.name });
+    return res.status(204).json();
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err);
+  }
+});
+
+app.post("/api/user", async (req, res) => {
+  const {
+    userID,
+    name,
+    email,
+    password,
+    city,
+    state,
+    country,
+    address,
+    zipcode,
+    cartID,
+    subscriptionID,
+  } = req.body;
+  try {
+    await db
+      .collection("users")
+      .doc("/" + userID + "/")
+      .create({
+        name,
+        email,
+        password,
+        city,
+        state,
+        country,
+        address,
+        zipcode,
+        cartID,
+        subscriptionID,
+      });
+    return res.status(200).json({
+      message: "user created successfully",
+    });
+  } catch {
+    return res.status(500).json({
+      err,
+    });
+  }
+});
 
 exports.app = onRequest(app);
-
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
