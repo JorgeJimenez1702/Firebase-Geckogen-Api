da29a0fc83c3cd816f4f8215fe33c1a1f3a8cc7d
Polo1017 <A01253696@itesm.mx>
2023-11-29 10:28:22 -0700

    Terrarium implementation changes

--- functions/index.js
+++ functions/index.js
@@ -333,6 +333,46 @@ app.get("/api/mygeckos/:userID", async (req, res) => {
   }
 });
 
+app.post("/api/terrariums", async (req, res) => {
+  try {
+    await db.collection("terrariums").doc().create({
+      id: req.body.id,
+      Name: req.body.name,
+      Humidity: req.body.humidity,
+      Light_Cycle: req.body.light_cycle,
+      Oxigenation: req.body.oxigenation,
+      Temperature: req.body.temperature,
+    });
+
+    return res.status(204).json();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
+
+app.get("/api/terrariums/", async (req, res) => {
+  try {
+    const query = db.collection("terrariums");
+    const querySnapshot = await query.get();
+    const docs = querySnapshot.docs;
+
+    const response = docs.map((doc) => ({
+      id: doc.data().id,
+      Name: doc.data().Name,
+      Humidity: doc.data().Humidity,
+      Light_Cycle: doc.data().Light_Cycle,
+      Oxigenation: doc.data().Oxigenation,
+      Temperature: doc.data().Temperature,
+    }));
+
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
+
 exports.api = onRequest(app);
 
 // Create and deploy your first functions
